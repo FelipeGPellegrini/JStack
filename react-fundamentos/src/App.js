f127a7023af6aa876325f1b43597b1089ec8cced
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2022-12-02 10:17:57 -0300

    react

---
+++ react-fundamentos/src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+const category = 'Posts da semana';
+
+function App() {
+    return (
+        <Fragment>
+            <h1>Jstack's Blog</h1>
+            <h2>{category}</h2>
+        </Fragment>
+    );
+};
+
+export default App;
