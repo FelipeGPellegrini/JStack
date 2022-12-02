39ebfe26c941e5c39d8dac1d1dfa71a62d5dc2a1
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2022-12-02 11:47:07 -0300

    update

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,14 +1,39 @@
-import React, { Fragment } from "react";
+import React, { Fragment } from 'react'
+import Post from './Post'
 
-const category = 'Posts da semana';
 
-function App() {
-    return (
-        <Fragment>
-            <h1>Jstack's Blog</h1>
-            <h2>{category}</h2>
-        </Fragment>
-    );
+// Props -> Propriedades
+function App () {
+  return (
+    <Fragment>
+      <h1>JStack's Blog</h1>
+      <h2>Posts da semana</h2>
+
+      <hr />
+
+      <Post post={{
+        title:"Titulo da notícia 01",
+        subtitle:"Subtítulo da notícia 01"
+      }}
+      
+      />
+
+      <Post post={{
+        title:"Titulo da notícia 02",
+        subtitle:"Subtítulo da notícia 02"
+      }}
+      
+      />
+
+      <Post post={{
+        title:"Titulo da notícia 03",
+        subtitle:"Subtítulo da notícia 03"
+      }}
+      
+      />
+
+    </Fragment>
+  )
 };
 
-export default App;
+export default App
