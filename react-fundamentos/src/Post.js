11391958e291f43251d91778f915efddd1903028
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2022-12-07 10:19:04 -0300

    read

--- react-fundamentos/src/Post.js
+++ react-fundamentos/src/Post.js
@@ -2,17 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 export default function Post(props){
- return (
+
+    return (
     <>
-        <article>
-            <strong>{props.post.title}</strong>
+{   
+    <>
+      <article>
+            <strong>
+                {props.post.read ? <s>{props.post.title}</s>: props.post.title}
+            </strong>
             <button onClick={() => props.onRemove(props.post.id)}>Remover</button>
             <br/>
             <small>{props.post.subtitle}</small>
+            Media: {props.post.likes / 2}
         </article>
-        Media: {props.likes / 2}
+        
         <br/>
-        <br />
+        <br/>
+    </>
+}
         
     </>
  )
@@ -22,8 +30,9 @@ Post.propTypes = {
    post: PropTypes.shape({
         id: PropTypes.number.isRequired,
         title: PropTypes.string.isRequired,
-        subtitle: PropTypes.string.isRequired
+        subtitle: PropTypes.string.isRequired,likes: PropTypes.number.isRequired,
+        read: PropTypes.bool.isRequired
     }).isRequired, 
-    likes: PropTypes.number.isRequired,
+    
     onRemove: PropTypes.func.isRequired
 }
