3c6fc5373f7eed8dd19548b1f3fa187206e299e3
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2022-12-15 11:12:41 -0300

    study

---
+++ react-fundamentos/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useMemo } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyle from './styles/global';
+import Layout from './components/Layout';
+
+import themes from './styles/themes/index';
+
+function App() {
+  const [theme, setTheme] = useState('dark');
+
+  const currentTheme = useMemo(() => {
+    return themes[theme] || themes.dark;
+  }, [theme]);
+
+  function handleToggleTheme() {
+    setTheme(prevStates => prevStates == 'dark' ? 'light' : 'dark')
+  }
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <GlobalStyle />
+      <Layout onToggleTheme={handleToggleTheme}
+      selectedTheme={theme}
+      />
+    </ThemeProvider>
+  );
+};
+
+export default App;
